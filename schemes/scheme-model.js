1f62c817afe51daf231834e01eca5532e1763cd0
Arvin Agas <arvagas@gmail.com>
2019-09-11 17:23:14 -0400

    findById fn functional

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,9 +2,17 @@ const db = require('../data/db-config')
 
 module.exports = {
     find,
+    findById,
 }
 
 function find() {
     return db('schemes')
     .then(schemes => schemes)
+}
+
+function findById(id) {
+    return db('schemes')
+    .where({ id })
+    .first()
+    .then(scheme => scheme)
 }
